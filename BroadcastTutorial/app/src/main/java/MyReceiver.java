76417883bc348242cd14a4a2979735de588972f8
cworkman123 <cworkman@channingworkman.com>
2014-11-23 13:33:54 -0500

    Add Receiver Class and Register Receiver

---
+++ BroadcastTutorial/app/src/main/java/MyReceiver.java
@@ -0,0 +1,15 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Channing on 11/23/2014.
+ */
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+    }
+}
